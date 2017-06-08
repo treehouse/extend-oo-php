2fa20fa6bc77da689e9f241c2bd1c778d775e6bc
Alena Holligan <alena@holligan.us>
2017-06-08 10:16:16 -0700

    Extended Directory after Completed Course

---
+++ extended-directory/classes/ListingInactive.php
@@ -0,0 +1,23 @@
+<?php
+
+class ListingInactive extends ListingBasic
+{
+    protected $status = 'inactive';
+    
+    public function getTitle()
+    {
+        return '<strike>' . parent::getTitle() . '</strike>';
+    }
+    public function getWebsite()
+    {
+        return;
+    }
+    public function getEmail()
+    {
+        return;
+    }
+    public function getTwitter()
+    {
+        return;
+    }
+}
